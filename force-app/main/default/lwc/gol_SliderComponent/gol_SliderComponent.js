a34da88026418415f965a0b0bd9ac43029a7180b
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2024-12-18 09:17:17 +0000

    OXO Comp Good shape  updated

---
+++ force-app/main/default/lwc/gol_SliderComponent/gol_SliderComponent.js
@@ -0,0 +1,18 @@
+import { LightningElement, track } from 'lwc';
+ 
+export default class SliderComponent extends LightningElement {
+    @track sliderValue = 50000;
+    minValue = 0;
+    maxValue = 100000;
+ 
+    handleInputChange(event) {
+        let value = Number(event.target.value);
+        if (value >= this.minValue && value <= this.maxValue) {
+            this.sliderValue = value;
+        }
+    }
+ 
+    handleSliderChange(event) {
+        this.sliderValue = event.target.value;
+    }
+}

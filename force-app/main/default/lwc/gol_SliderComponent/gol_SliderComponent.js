e5461680f4242f9e81fe4673d218db1d1ac10f0c
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-01-02 16:51:11 +0000

    All good

--- force-app/main/default/lwc/gol_SliderComponent/gol_SliderComponent.js
+++ force-app/main/default/lwc/gol_SliderComponent/gol_SliderComponent.js
@@ -6,13 +6,24 @@ export default class SliderComponent extends LightningElement {
     maxValue = 100000;
  
     handleInputChange(event) {
-        let value = Number(event.target.value);
+        let value = Number(event.target.value.replace(/,/g, ''));
         if (value >= this.minValue && value <= this.maxValue) {
             this.sliderValue = value;
         }
     }
+
+    get formattedSliderValue() {
+        return this.sliderValue.toLocaleString('en-US');
+    }
+
+    get formattedMaxValue() {
+        return this.maxValue.toLocaleString('en-US');
+    }
  
     handleSliderChange(event) {
-        this.sliderValue = event.target.value;
+        this.sliderValue = Number(event.target.value);
+        const percentage = ((this.sliderValue - this.minValue) / (this.maxValue - this.minValue)) * 100;
+        const slider = this.template.querySelector('.custom-slider');
+        slider.style.background = `linear-gradient(to right, rgb(12 18 28 / 90%) ${percentage}%, rgb(12 18 28 / 20%) ${percentage}%)`;
     }
 }

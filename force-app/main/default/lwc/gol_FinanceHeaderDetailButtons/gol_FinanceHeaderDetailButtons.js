34fb316210797cc20ec9e9eb08f2ca70add712c7
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-01-22 10:18:25 +0000

    All  Sprint 1 loaded

--- force-app/main/default/lwc/gol_FinanceHeaderDetailButtons/gol_FinanceHeaderDetailButtons.js
+++ force-app/main/default/lwc/gol_FinanceHeaderDetailButtons/gol_FinanceHeaderDetailButtons.js
@@ -3,15 +3,19 @@ import { FlowAttributeChangeEvent, FlowNavigationNextEvent, FlowNavigationBackEv
 import 	GOL_Finance_Finance_calculator from '@salesforce/label/c.GOL_Finance_Finance_calculator';
 import 	GOL_Finance_Personal_Purchase from '@salesforce/label/c.GOL_Finance_Personal_Purchase';
 import 	GOL_Finance_Purchase_For_a_Company from '@salesforce/label/c.GOL_Finance_Purchase_For_a_Company';
+import 	GOL_Finance_Value_PRIVATE from '@salesforce/label/c.GOL_Finance_Value_PRIVATE';
+import 	GOL_Finance_Value_PROFESSIONAL from '@salesforce/label/c.GOL_Finance_Value_PROFESSIONAL';
 export default class Gol_FinanceHeaderDetailButtons extends LightningElement {
-    @api companyNameCheck = 'PRIVATE';
+    @api companyNameCheck;
     @api quoteId;
     @api loadButtonChecks;
     @api productAPIResponseCode;
     label = {
         GOL_Finance_Finance_calculator,
         GOL_Finance_Personal_Purchase,
-        GOL_Finance_Purchase_For_a_Company
+        GOL_Finance_Purchase_For_a_Company,
+        GOL_Finance_Value_PRIVATE,
+        GOL_Finance_Value_PROFESSIONAL
      }
     @track personalPerchaseStyle = 'btn-client-deselect';
     @track companyPerchaseStyle = 'btn-client-select';
@@ -19,11 +23,11 @@ export default class Gol_FinanceHeaderDetailButtons extends LightningElement {
     connectedCallback() {
         //this.dispatchEvent(new FlowAttributeChangeEvent('companyNameCheck', 'PRIVATE')); 
         //this.navigateFlow();
-        console.log('this.companyNameCheck==>'+this.companyNameCheck);
-        if(this.companyNameCheck === 'PROFESSIONAL'){
+        console.log(this.label.GOL_Finance_Value_PRIVATE+'<==this.companyNameCheck==>'+this.companyNameCheck);
+        if(this.companyNameCheck === this.label.GOL_Finance_Value_PROFESSIONAL){
             this.personalPerchaseStyle = 'btn-client-select';
             this.companyPerchaseStyle = 'btn-client-deselect';
-        }else if(this.companyNameCheck === 'PRIVATE'){
+        }else if(this.companyNameCheck === this.label.GOL_Finance_Value_PRIVATE){
             this.personalPerchaseStyle = 'btn-client-deselect';
             this.companyPerchaseStyle = 'btn-client-select';
         }
@@ -38,7 +42,7 @@ export default class Gol_FinanceHeaderDetailButtons extends LightningElement {
                   this.personalPerchaseStyle = 'btn-client-deselect';
                   this.companyPerchaseStyle = 'btn-client-select';
               }
-              console.log('--------<<>>>'+event.target.value);
+            console.log('--------<<>>>'+event.target.value);
             this.dispatchEvent(new FlowAttributeChangeEvent('companyNameCheck', event.target.value));
             this.navigateFlow();
             }
---
+++ force-app/main/default/lwc/gol_TabSetComponent/gol_TabSetComponent.js
@@ -0,0 +1,230 @@
+import { LightningElement, api, track, wire} from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import { publish, MessageContext } from 'lightning/messageService';
+import TAB_CHANGE_MESSAGE_CHANNEL from '@salesforce/messageChannel/SummaryScreenTabChange__c';
+import GOL_F1_SUMMARY from '@salesforce/label/c.GOL_F1_SUMMARY';
+import GOL_F1_ACCESSORIES from '@salesforce/label/c.GOL_F1_ACCESSORIES';
+import GOL_F1_ADD_ONS from '@salesforce/label/c.GOL_F1_ADD_ONS';
+import GOL_F1_EXTRAS from '@salesforce/label/c.GOL_F1_EXTRAS';
+import GOL_F1_TRADE_IN from '@salesforce/label/c.GOL_F1_TRADE_IN';
+import GOL_F1_FINANCING from '@salesforce/label/c.GOL_F1_FINANCING';
+import GOL_F1_ADMINISTRATION from '@salesforce/label/c.GOL_F1_ADMINISTRATION';
+
+
+export default class Gol_TabSetComponent extends LightningElement {
+  @api quoteId;
+  @api retUrl;
+  @api readOnly;
+  @api showFinancingTab;        //GOL-2142
+  @track tabItems = [];         //GOL-2142
+  // currentPageReference = null; 
+
+  @wire(MessageContext)
+  messageContext;
+
+
+  @wire(CurrentPageReference)
+  currentPageReference;
+
+  connectedCallback() {
+    console.log('====<<>>>>'+this.showFinancingTab)
+    console.log('====<<>>>>'+this.quoteId)
+    this.tabSetItemshandler();        //GOL-2142
+    console.log('current ref : ' + JSON.stringify(this.currentPageReference));     
+    console.log(window.location.href);
+    console.log(`c__quoteId = ${this.currentPageReference.state.defaulttab}`); 
+    this.defaulttab = this.currentPageReference.state.defaulttab;
+    console.log('default Tab : ' + this.defaulttab);
+    
+    if (this.defaulttab === undefined) {
+      this.defaulttab = 'tab-default-Summary';
+    }
+    this.tabItems.forEach(tab => {
+      if(tab.TabId === this.defaulttab){
+        tab.IsActive = true;
+        tab.ItemClass = 'slds-tabs_default__item slds-is-active';
+        tab.ContentClass = 'slds-tabs_default__content slds-show';
+      } else{
+        tab.IsActive = false;
+        tab.ItemClass = 'slds-tabs_default__item';
+        tab.ContentClass = 'slds-tabs_default__content slds-hide';
+      }
+    });
+  }
+
+  //pass input variables to the flow when component is loaded
+  get summaryTabinputVariables() {
+    return [
+      {
+        name: 'quoteId',
+        type: 'String',
+        value: this.quoteId
+      },
+      {
+        name: 'readOnly',
+        type: 'Boolean',
+        value: this.readOnly
+      },
+      {
+        name: 'retUrl',
+        type: 'String',
+        value: this.retUrl
+      }
+    ];
+  }
+
+  //GOL-2142 start
+  tabSetItemshandler() {
+    this.tabItems.push(
+      {
+        Title: GOL_F1_SUMMARY,
+        HeaderFlowName: "GOL_Screen_Flow_Summary_Tab_Header_Car_Image",
+        FlowName: "GOL_Screen_Flow_Summary_Tab",
+        ItemClass: 'slds-tabs_default__item slds-is-active',
+        ContentClass: 'slds-tabs_default__content slds-show',
+        TabItemStyle: '',
+        TabIndex: 0,
+        IsActive: true,
+        TabId: 'tab-default-Summary',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      },
+      {
+        Title: GOL_F1_ACCESSORIES,
+        HeaderFlowName: "",
+        FlowName: "GOL_Screen_Flow_Accessories_Tab",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle: '',
+        TabIndex: 1,
+        IsActive: false,
+        TabId: 'tab-default-Accessories',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      },
+      {
+        Title: GOL_F1_ADD_ONS,
+        HeaderFlowName: "",
+        FlowName: "GOL_Screen_Flow_AddOnsTab",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle: '',
+        TabIndex: 2,
+        IsActive: false,
+        TabId: 'tab-default-AddOns',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      },
+      {
+        Title: GOL_F1_EXTRAS,
+        HeaderFlowName: "",
+        FlowName: "GOL_Screen_Flow_Extras_Tab",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle: '',
+        TabIndex: 3,
+        IsActive: false,
+        TabId: 'tab-default-Extras',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      },
+      {
+        Title: GOL_F1_TRADE_IN,
+        HeaderFlowName: "",
+        FlowName: "W_I_Trade_In",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle: '',
+        TabIndex: 4,
+        IsActive: false,
+        TabId: 'tab-default-TradeIn',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      });
+
+      if (this.showFinancingTab) {
+        this.tabItems.push({
+        Title: GOL_F1_FINANCING,
+        HeaderFlowName: "",
+        FlowName: "GOL_Screen_Flow_Finance_Tab",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle: '',
+        TabIndex: 5,
+        IsActive: false,
+        TabId: 'tab-default-Financing',
+        IsFinanceTab:true,
+        IsTradeInTab:false
+      },
+      {
+        Title: GOL_F1_ADMINISTRATION,
+        HeaderFlowName: "",
+        FlowName: "W_G_Screen_Flow_Administration_Tab",
+        ItemClass: 'slds-tabs_default__item',
+        ContentClass: 'slds-tabs_default__content slds-hide',
+        TabItemStyle : 'margin-left: auto;margin-right:20px',
+        TabIndex: 6,
+        IsActive: false,
+        TabId: 'tab-default-Administration',
+        IsFinanceTab:false,
+        IsTradeInTab:false
+      });
+      } else {
+        this.tabItems.push({
+          Title: GOL_F1_ADMINISTRATION,
+          HeaderFlowName: "",
+          FlowName: "W_G_Screen_Flow_Administration_Tab",
+          ItemClass: 'slds-tabs_default__item',
+          ContentClass: 'slds-tabs_default__content slds-hide',
+          TabItemStyle : 'margin-left: auto;margin-right:20px',
+          TabIndex: 6,
+          IsActive: false,
+          TabId: 'tab-default-Administration',
+          IsFinanceTab:false,
+          IsTradeInTab:false
+        });
+      }
+  }
+  //GOL-2142 ends
+
+  handleTabClick(event) {
+    const selectedTabId = event.currentTarget.dataset.tab;
+    if(selectedTabId == 'tab-default-Summary') {
+      const message = {
+        data: {
+          tabInfo : 'Summary Tab Clicked'
+        },
+      };
+      publish(this.messageContext, TAB_CHANGE_MESSAGE_CHANNEL, message);
+    }
+    this.tabItems.forEach(tab => {
+      if(tab.TabId === selectedTabId) {
+        // this.defaulttab = tab.TabId;
+        tab.IsActive = true;
+        tab.ItemClass = 'slds-tabs_default__item slds-is-active';
+        tab.ContentClass = 'slds-tabs_default__content slds-show';
+      } else {
+        // this.defaulttab = 'tab-default-Summary';
+        tab.IsActive = false;
+        tab.ItemClass = 'slds-tabs_default__item';
+        tab.ContentClass = 'slds-tabs_default__content slds-hide';
+      }
+    });
+  }
+
+  handleStatusChange(event) {
+      //executes when finish event is triggered in the lightning flow
+    if(event.detail.status === 'FINISHED'){
+        /*const outputVariables = event.detail.outputVariables;
+        for(let i = 0; i < outputVariables.length; i++) {
+              const outputVar = outputVariables[i];
+              if(outputVar.name == 'Summary'){
+                console.log('Summary from flow >> ' + outputVar.value);
+              }
+              if(outputVar.name == 'Summary'){
+                console.log('Summary from flow >> ' + outputVar.value);
+              }
+        } */
+    }
+  }
+}

661ed40375225d884dab548a42bc2a0272fdfa2c
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-01-23 11:51:33 +0000

    Selected Data is loading

--- force-app/main/default/lwc/gol_parentSliderComponent/gol_parentSliderComponent.js
+++ force-app/main/default/lwc/gol_parentSliderComponent/gol_parentSliderComponent.js
@@ -3,11 +3,12 @@ import GOL_Select_Financial_Product from '@salesforce/label/c.GOL_Select_Financi
 import GOL_Adjust_parameters from '@salesforce/label/c.GOL_Adjust_parameters';
 import GOL_Calculate_Financing from '@salesforce/label/c.GOL_Calculate_Financing';
 import GOL_Finance_Insurance_and_Services from '@salesforce/label/c.GOL_Finance_Insurance_and_Services';
-import { NavigationMixin } from 'lightning/navigation';
+// import { NavigationMixin } from 'lightning/navigation';
 import { FlowAttributeChangeEvent, FlowNavigationNextEvent, FlowNavigationBackEvent, FlowNavigationFinishEvent } from 'lightning/flowSupport';
 
 export default class gol_parentSliderComponent extends LightningElement {
   @api response;
+  @api serializedData;
   //   isSubmitted = false;
   sliders = [];
   namesWithIds = [];
@@ -260,24 +261,34 @@ export default class gol_parentSliderComponent extends LightningElement {
     this.checkIfAllValuesSelected();
   }
 
-  checkIfAllValuesSelected() {
+  updateFlowVariables() {
     const serializedData = {
         downpayment: this.downpayment,
         mileage: this.mileage,
         duration: this.duration
     };
     console.log("Selected Data:", JSON.stringify(serializedData, null, 2));
-    this[NavigationMixin.Navigate]({
-      type: 'standard__flow',
-      attributes: {
-          flowApiName: 'GOL_Screen_Flow_Finance_Tab'
-      },
-    });
-    this.dispatchEvent(new FlowAttributeChangeEvent('calculateFinanceButton', event.target.value));
-    this.navigateFlow();
+    // this[NavigationMixin.Navigate]({
+    //   type: 'standard__flow',
+    //   attributes: {
+    //       flowApiName: 'GOL_Screen_Flow_Finance_Tab'
+    //   },
+    // });
+    this.dispatchEvent(
+      new FlowAttributeChangeEvent('serializedData', JSON.stringify(serializedData))
+    );
   }
 
-  navigateFlow() {
+  handleCalculateFinancingClick() {
+    this.updateFlowVariables();
     this.dispatchEvent(new FlowNavigationNextEvent());
+  }
+
+  handleBackToFianceCalculator() {
+    this.dispatchEvent(new FlowNavigationBackEvent());
+  }
+
+  handleBackToFianceFinish() {
+    this.dispatchEvent(new FlowNavigationFinishEvent());
   }
 }

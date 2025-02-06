3855e710fdcda7a9d5723098ce3835e1eba1d812
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-02-06 14:27:14 +0000

    Working Mode EOD

--- force-app/main/default/lwc/golFinanceDetails/golFinanceDetails.js
+++ force-app/main/default/lwc/golFinanceDetails/golFinanceDetails.js
@@ -1,6 +1,20 @@
 import { LightningElement, api } from 'lwc';
+import { FlowAttributeChangeEvent, FlowNavigationNextEvent, FlowNavigationBackEvent, FlowNavigationFinishEvent } from 'lightning/flowSupport';
+
 
 export default class golFinanceDetails extends LightningElement {
     @api financeRecord;
     @api label;
+
+    handleModifyClick() {
+        console.log('Modify button clicked for JLR ID:', JSON.stringify(this.financeRecord, null, 2));
+        const modifyFSRecordEvent = new CustomEvent('modify', {
+            detail: {
+                financeId: this.financeRecord?.GOL_JLR_ID__c
+            }
+        });
+        const flowNavigationEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(flowNavigationEvent);
+        this.dispatchEvent(modifyFSRecordEvent);
+    }
 }

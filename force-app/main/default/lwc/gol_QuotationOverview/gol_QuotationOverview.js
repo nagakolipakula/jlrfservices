1b723c770d07e2a55e75afd636d130a4e8eb5423
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-02-12 13:31:22 +0000

    Working Good with flow v90 created 05022025 1522

---
+++ force-app/main/default/lwc/gol_QuotationOverview/gol_QuotationOverview.js
@@ -0,0 +1,38 @@
+import { LightningElement } from 'lwc';
+import GOL_Quotation_overview from '@salesforce/label/c.GOL_Quotation_overview';
+import GOL_New_Calculation_Button from '@salesforce/label/c.GOL_New_Calculation_Button';
+import GOL_Update_Quote_Button from '@salesforce/label/c.GOL_Update_Quote_Button';
+import GOL_Open_Quote_Button from '@salesforce/label/c.GOL_Open_Quote_Button';
+import GOL_Send_To_Bank_Button from '@salesforce/label/c.GOL_Send_To_Bank_Button';
+import GOL_Link_To_Pf_PoS from '@salesforce/label/c.GOL_Link_To_Pf_PoS';
+import GOL_Link_To_Arval_Pos from '@salesforce/label/c.GOL_Link_To_Arval_Pos';
+
+export default class QuotationOverview extends LightningElement {
+    label = {
+        GOL_Quotation_overview,
+        GOL_New_Calculation_Button,
+        GOL_Update_Quote_Button,
+        GOL_Open_Quote_Button,
+        GOL_Send_To_Bank_Button,
+        GOL_Link_To_Pf_PoS,
+        GOL_Link_To_Arval_Pos
+    }
+
+    handleNewCalculationClick(){
+
+    }
+
+    handleUpdateClick()
+    {
+        
+    }
+    handleOpenClick()
+    {
+
+    }
+
+    handleSendToBankClick()
+    {
+
+    }
+}

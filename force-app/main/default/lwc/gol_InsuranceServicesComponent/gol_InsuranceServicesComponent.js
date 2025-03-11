92f47921f41f145473fa1352e4a4ec103837f6c4
Naga Kolipakula <naga.kolipakula@devops.fco.gov.uk>
2025-03-11 09:24:00 +0000

    04032025

--- force-app/main/default/lwc/gol_InsuranceServicesComponent/gol_InsuranceServicesComponent.js
+++ force-app/main/default/lwc/gol_InsuranceServicesComponent/gol_InsuranceServicesComponent.js
@@ -1,8 +1,55 @@
 import { LightningElement, api } from 'lwc';
 import 	GOL_Finance_Insurance_and_Services from '@salesforce/label/c.GOL_Finance_Insurance_and_Services';
+import 	GOL_Finance_Insurance_product_1 from '@salesforce/label/c.GOL_Finance_Insurance_product_1';
+import 	GOL_Finance_Insurance_product_2 from '@salesforce/label/c.GOL_Finance_Insurance_product_2';
 export default class Gol_InsuranceServicesComponent extends LightningElement {
-    @api indexnumber;
+    
+    @api insuranceProducts;
+    cpiProducts = [];
+    nonCpiProducts = [];
+    isInsuranceProducts = {'cpiProducts':false,'nonCpiProducts':false,'services':false};
     label = {
-        GOL_Finance_Insurance_and_Services
+        GOL_Finance_Insurance_and_Services,
+        GOL_Finance_Insurance_product_1,
+        GOL_Finance_Insurance_product_2
+    }
+    connectedCallback(){
+        console.log('MS++ Insurance Products==> ',JSON.stringify(this.insuranceProducts));
+        if(this.insuranceProducts){
+            this.insuranceProductsCheck();
+        }
+    }
+    insuranceProductsCheck(){
+        if(this.insuranceProducts.cpiProducts){
+            this.isInsuranceProducts.cpiProducts = true;
+            // this.insuranceProducts.cpiProducts.forEach(element => {
+            //     element.checked = false;
+            //     // if(element.description !== undefined){
+            //     //     element.checkboxLabel = element.description;
+            //     // }else if(element.name){
+            //     //     element.checkboxLabel = element.name;
+            //     // }else{
+            //     //     element.checkboxLabel = '';
+            //     // }
+            // });
+            this.cpiProducts = this.insuranceProducts.cpiProducts;      
+          }         
+        if(this.insuranceProducts.nonCpiProducts){
+            this.isInsuranceProducts.nonCpiProducts = true;
+            // this.insuranceProducts.nonCpiProducts.forEach(element => {
+            //     element.checked = false;
+            //     if(element.description !== undefined){
+            //         element.checkboxLabel = element.description;
+            //     }else if(element.name){
+            //         element.checkboxLabel = element.name;
+            //     }else{
+            //         element.checkboxLabel = '';
+            //     }
+            // });
+            this.nonCpiProducts = this.insuranceProducts.nonCpiProducts;
+          }       
+        if(this.insuranceProducts.inputFields &&  this.insuranceProducts.inputFields.services){
+            this.isInsuranceProducts.services = true;
+          }
     }
 }

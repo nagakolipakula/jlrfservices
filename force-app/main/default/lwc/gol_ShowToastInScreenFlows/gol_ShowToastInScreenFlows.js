e9661a6148621f14ef7ccc6282b317a93c06d7e9
Naga Kolipakula <anilkittuk@outlook.com>
2025-03-28 11:57:50 +0000

    Good Shape2803

---
+++ force-app/main/default/lwc/gol_ShowToastInScreenFlows/gol_ShowToastInScreenFlows.js
@@ -0,0 +1,27 @@
+import { LightningElement, api,track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import GOL_SuccessConfirmationIcon from '@salesforce/resourceUrl/GOL_SuccessConfirmationIcon';
+
+export default class Gol_ShowToastInScreenFlows extends LightningElement {
+
+    @api title;
+
+    @track isVisible;
+
+    connectedCallback() {
+        console.log('------<<<>>>>Under the message'+this.title)
+        // Set a timeout to hide the component after 10 seconds
+        if(this.title != '' && this.title != null && this.title != undefined){
+            this.isVisible = true;
+            setTimeout(() => {
+                this.isVisible = false;
+            }, 10000);
+        }
+        console.log('------<<<>>>>Under the message')
+    }
+
+    get iconUrl() {
+        return GOL_SuccessConfirmationIcon; // Adjust the file name and extension accordingly
+    }
+
+}

84bd470f2de4f6902cb44ffb398a22bbb770ec05
Naga Kolipakula <anilkittuk@outlook.com>
2025-04-04 10:13:42 +0100

    Good Shape2803

--- force-app/main/default/lwc/gol_parentSliderComponent/gol_parentSliderComponent.js
+++ force-app/main/default/lwc/gol_parentSliderComponent/gol_parentSliderComponent.js
@@ -7,6 +7,8 @@ import GOL_Finance_Insurance_and_Services from '@salesforce/label/c.GOL_Finance_
 import GOL_Amount_incl_VAT from '@salesforce/label/c.GOL_Amount_incl_VAT';
 import { FlowAttributeChangeEvent, FlowNavigationNextEvent, FlowNavigationBackEvent, FlowNavigationFinishEvent } from 'lightning/flowSupport';
 import getInputFieldsMappingRecords from '@salesforce/apex/GOL_GetFinanceQuote.getInputFieldsMappingRecords';
+import GOL_Go_To_Overview from '@salesforce/label/c.GOL_Go_To_Overview';
+
 
 export default class gol_parentSliderComponent extends LightningElement {
   @api buttonActionForOverview;
@@ -29,6 +31,7 @@ export default class gol_parentSliderComponent extends LightningElement {
   @api IsUpdateRetailerDiscount = false;
   @api retailerDiscountSerializedData;
   @api clickedButtonName;
+  @api FinRecsCheckForOverview;
   retailerDiscountInputFiledAry = [];
   retailerDiscountSelectedProductId;
   selectedSliderValues = new Map();
@@ -47,10 +50,12 @@ export default class gol_parentSliderComponent extends LightningElement {
     GOL_Adjust_parameters,
     GOL_Calculate_Financing,
     GOL_Finance_Insurance_and_Services,
-    GOL_Amount_incl_VAT
+    GOL_Amount_incl_VAT,
+    GOL_Go_To_Overview
   }
   
   connectedCallback() {
+    console.log('Fin Info Records NK ==> '+this.FinRecsCheckForOverview);
     console.log('Logged in user details==>'+JSON.stringify(this.UserDetails,null,2));
      console.log('Finance Item==> '+JSON.stringify(this.financeitem,null,2));
     // console.log('First Finance Info Record:', this.ContactId);
@@ -94,6 +99,16 @@ export default class gol_parentSliderComponent extends LightningElement {
       console.error('Raw Response:', this.response);
       this.hasNoFinancialProducts = true;
     }
+  }
+
+  get isFinRecsCheckForOverview() {
+    if (this.FinRecsCheckForOverview === true || this.FinRecsCheckForOverview === 'true') {
+      return true;
+    }
+    if (this.FinRecsCheckForOverview === false || this.FinRecsCheckForOverview === 'false') {
+      return false;
+    }
+    return false;
   }
 
   // removeSetFlags(data) {
@@ -945,4 +960,11 @@ handleInsuranceProductChange(event){
   //   });
   //   console.log(inputFields);
   // }
+
+  handleGoToOverview(){
+    const action = new FlowAttributeChangeEvent('buttonActionForOverview', 'goToOverviewButtonClick');
+    this.dispatchEvent(action);
+    const nextEvent = new FlowNavigationNextEvent();
+    this.dispatchEvent(nextEvent);
+  }
 }
